8a4b16034fdc42d99c33a48951072c2a47ae7da9
ronakkoladiya92 <ronakkoladiya0010@gmail.com>
2023-06-04 10:41:28 +0530

    msg

---
+++ lib/first.dart
@@ -0,0 +1,8 @@
+class first
+{
+  void get()
+  {
+    print("first file");
+    print("hi");
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+   print("Git Demo");
+   print("Hello good morning");
+   print("Jenish Changes");
+   print("Hello Good Afternoon");
+}
